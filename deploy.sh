94fedb45d627a52ef22f519f439858613f617f5c
JeongUk Park <dnr1105@gmail.com>
2022-08-05 01:17:38 +0900

    Update Repo

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+yarn docs:build
 
 # navigate into the build output directory
 cd docs/.vuepress/dist
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/bin-e/TIL master:gh-pages
+git push -f https://github.com/bin-e/TIL.git master:gh-pages
 
 cd -
